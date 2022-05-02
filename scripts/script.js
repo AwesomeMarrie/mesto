80fc84500b4ec3f0fcc6547aebe38ae0cfe99ce0
Marina Kuzmina <mightymarrie@yandex.ru>
2022-05-02 22:07:14 +0300

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,28 @@
 let openPopUp = document.querySelector('.profile__info-button');
-let popUp = document.querySelector('.popup__container');
+let popUp = document.querySelector('.popup__container-form');
 let closePopUp = document.querySelector('.popup__close');
 let userName = document.querySelector('.profile__info-name');
 let userInfo = document.querySelector('.profile__info-about');
 let backGround = document.querySelector('.popup');
-let nameChange = document.querySelector('.popup__input_name');
-let infoChange = document.querySelector('.popup__input_about');
+let nameChange = document.querySelector('.popup__input_name_change');
+let infoChange = document.querySelector('.popup__input_about_change');
 
-function open() {
-    if (backGround.classList.contains('popup_opened')) {
-        backGround.classList.remove('popup_opened');
-    }else {
-        backGround.classList.add('popup_opened');
-    }
+function open(event) {
+    backGround.classList.add('popup_opened');
+    nameChange.value = userName.textContent;
+    infoChange.value = userInfo.textContent;
+    event.preventDefault();
 }
-
+function close() {
+    backGround.classList.remove('popup_opened');
+}
 openPopUp.addEventListener('click', open);
-closePopUp.addEventListener('click', open);
+closePopUp.addEventListener('click', close);
 
 function newName(evt) {
     userName.textContent = nameChange.value;
     userInfo.textContent = infoChange.value;
-    backGround.classList.remove('popup_opened');
     evt.preventDefault();
 }
 popUp.addEventListener('submit', newName);
+popUp.addEventListener('submit', close);
