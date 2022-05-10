7a9884229f05b4b80923456a8eea3cec7cdb9824
Marina Kuzmina <mightymarrie@yandex.ru>
2022-05-10 14:29:25 +0300

    Massive photo

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//popUpEdit
 let openPopUp = document.querySelector('.profile__info-button');
 let popUp = document.querySelector('.popup__container-form');
 let closePopUp = document.querySelector('.popup__close');
@@ -25,3 +26,41 @@ function newName(evt) {
     evt.preventDefault();
 }
 popUp.addEventListener('submit', newName);
+
+//cards
+const containerCard = document.querySelector('.photo');
+const templateCard = document.querySelector('#card').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+initialCards.forEach(function newCard(element) {
+  const editCard = templateCard.cloneNode(true);
+  editCard.querySelector('.card__tile-name').textContent = element.name;
+  editCard.querySelector('.card__image').src = element.link;
+
+  containerCard.append(editCard);
+  return editCard
+});
